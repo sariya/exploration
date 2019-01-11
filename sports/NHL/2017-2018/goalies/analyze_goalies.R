65c2e26c42cbd1e09c5b384d69c004851a7a6d84
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-01-10 22:16:33 -0500

    getting acquainted with basic columns

--- sports/NHL/2017-2018/goalies/analyze_goalies.R
+++ sports/NHL/2017-2018/goalies/analyze_goalies.R
@@ -4,9 +4,33 @@
 #01/10/2019
 
 library(readxl)
-
+library(dplyr)
 
 file.goalies<-"/home/ss5505/data_exploration/sports/NHL/2017-2018/goalies/NHLGoalies2017-18.xls"
 workbooks<-excel_sheets(path = file.goalies)
 
 goaliesdf<-read_excel(file.goalies, sheet = "Goalies")
+
+print(dim(goaliesdf))
+##95 133
+
+print(length(unique(goaliesdf$"Team(s)"))) #get count of teams
+
+##--get count of people from different countries
+as.data.frame(count(goaliesdf,Cntry))
+
+##--see if any golie has done any its own goal
+
+unique(sort(goaliesdf$G))
+
+##--saves by any goalie
+
+goaliesdf$SV
+
+##--get save percentage
+goaliesdf$"SV%"
+
+##--get people who shoot
+
+as.data.frame(count(goaliesdf,Sh))
+
