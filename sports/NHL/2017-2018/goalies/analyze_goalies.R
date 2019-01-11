f102c10f41b233715801ebec58139a13499d233d
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-01-10 22:39:14 -0500

    cleaning understanding

--- sports/NHL/2017-2018/goalies/analyze_goalies.R
+++ sports/NHL/2017-2018/goalies/analyze_goalies.R
@@ -10,9 +10,24 @@ file.goalies<-"/home/ss5505/data_exploration/sports/NHL/2017-2018/goalies/NHLGoa
 workbooks<-excel_sheets(path = file.goalies)
 
 goaliesdf<-read_excel(file.goalies, sheet = "Goalies")
-
+goaliesdf<-as.data.frame(goaliesdf)
 print(dim(goaliesdf))
 ##95 133
+
+###get rid of duplicate columns
+
+
+##get rid of spaces in column names
+
+##get rid of () in column names
+
+##get rid of poor data
+
+##--get people who shoot
+as.data.frame(count(goaliesdf,Sh))
+goaliesdf<-goaliesdf[!is.na(goaliesdf$Sh),]
+
+as.data.frame(count(goaliesdf,Sh))
 
 print(length(unique(goaliesdf$"Team(s)"))) #get count of teams
 
@@ -20,17 +35,14 @@ print(length(unique(goaliesdf$"Team(s)"))) #get count of teams
 as.data.frame(count(goaliesdf,Cntry))
 
 ##--see if any golie has done any its own goal
-
 unique(sort(goaliesdf$G))
 
 ##--saves by any goalie
-
 goaliesdf$SV
 
 ##--get save percentage
 goaliesdf$"SV%"
 
-##--get people who shoot
 
-as.data.frame(count(goaliesdf,Sh))
+
 
